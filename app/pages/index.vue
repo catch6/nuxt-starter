8f6ae8736d082143e2f8a834331affe6273e57d3
zhanghao <catchlife6@163.com>
2025-11-07 09:37:50 +0800

    chore: 初始化 Nuxt 项目脚手架
    - 配置开发环境,包含 ESLint、Tailwind CSS 和 VSCode 推荐扩展
    - 集成 shadcn-vue UI 组件库和 Pinia 状态管理
    - 添加基础项目结构和依赖配置

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <Button>Click me</Button>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      // NUXT_PUBLIC_API_BASE
+      apiBase: '',
+    },
+  },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    '@nuxt/image',
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+  ],
+  shadcn: {
+    prefix: '',
+    componentDir: '~/components/ui'
+  },
+})
